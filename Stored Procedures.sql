a6505638a7758d49fe7bc8b91bf989e8366fa90c
Mathias Bærentsen <mathias.barentsen@gmail.com>
2019-04-27 15:15:51 +0200

    Add files via upload

--- Stored Procedures.sql
+++ Stored Procedures.sql
@@ -2,6 +2,7 @@
 Stored Procedures PROCESS
 */
 
+/* BeansProcessed */
 GO
 CREATE PROCEDURE BeansProcessed
 	(@Name NVARCHAR(50), @Weight FLOAT, @DateOfProcess DateTime2, @Loss FLOAT)
@@ -17,7 +18,7 @@ END
 Stored Procedures CUSTOMER
 */
 
-
+/* AddCustomer */
 GO
 CREATE PROCEDURE AddCustomer
 	(@CompanyName NVARCHAR(50), @ProductOwner NVARCHAR(50), @Address NVARCHAR(50), 
@@ -29,6 +30,7 @@ AS BEGIN
 	(@CompanyName, @ProductOwner, @Address, @Zip, @Town, @Telephone)
 END
 
+/* EditCustomer */
 GO
 CREATE PROCEDURE EditCustomer
 	(@ID INT, @CompanyName NVARCHAR(50), @ProductOwner NVARCHAR(50), @Address NVARCHAR(50), 
@@ -40,6 +42,7 @@ AS BEGIN
 	WHERE [ID] = @ID
 END
 
+/* DeleteCustomer */
 GO
 CREATE PROCEDURE DeleteCustomer
 	(@ID INT)
@@ -48,12 +51,34 @@ AS BEGIN
 	WHERE ID = @ID
 END
 
+/* ShowCustomers !!!! HER SKAL VI LIGE HAVE KIGGET PÅ NOGET MED HVAD DEN SKAL SØGE EFTER !!!! */
+GO
+CREATE PROCEDURE ShowCustomers
+	(@CompanyName NVARCHAR(50), @ProductOwner NVARCHAR(50), @Address NVARCHAR(50), 
+	@Zip NVARCHAR(10), @Town NVARCHAR(50), @Telephone NVARCHAR(20))
+AS BEGIN
+	SELECT [CompanyName], [ProductOwner], [Address], [Zip], [Town], [Telephone]
+	FROM ERP.Customer
+	WHERE [CompanyName] = @CompanyName
+END
+
+/* ShowCustomer */
+GO
+CREATE PROCEDURE ShowCustomer
+	(@ID INT)
+AS BEGIN
+	SELECT [CompanyName], [ProductOwner], [Address], [Zip], [Town], [Telephone]
+	FROM ERP.Customer 
+	WHERE [ID] = @ID
+END
+
 
 /*
 Stored Procedures RAWPRODUCT
 */
 
 
+/* AddRawProduct */
 GO
 CREATE PROCEDURE AddRawProduct
 	(@Name NVARCHAR(50), @Weight FLOAT, @DateOfPurchase DateTime2)
@@ -64,6 +89,7 @@ AS BEGIN
 	(@Name, @Weight, @DateOfPurchase)
 END
 
+/* EditRawProduct */
 GO
 CREATE PROCEDURE EditRawProduct
 	(@ID INT, @Name NVARCHAR(50), @Weight FLOAT, @DateOfPurchase DateTime2)
@@ -73,6 +99,7 @@ AS BEGIN
 	WHERE [ID] = @ID
 END
 
+/* DeleteRawProduct */
 GO
 CREATE PROCEDURE DeleteRawProduct
 	(@ID INT)
@@ -81,12 +108,32 @@ AS BEGIN
 	WHERE ID = @ID
 END
 
+/* ShowRawProducts !!!! HER SKAL VI LIGE HAVE KIGGET PÅ NOGET MED HVAD DEN SKAL SØGE EFTER !!!! */
+GO
+CREATE PROCEDURE ShowRawProducts
+	(@Name NVARCHAR(50), @Weight FLOAT, @DateOfPurchase DateTime2)
+AS BEGIN
+	SELECT [Name], [Weight], [DateOfPurchase]
+	FROM ERP.RawProduct
+	WHERE [Name] = @Name
+END
+
+/* ShowRawProduct */
+GO
+CREATE PROCEDURE ShowRawProduct
+	(@ID INT)
+AS BEGIN
+	SELECT [Name], [Weight], [DateOfPurchase]
+	FROM ERP.RawProduct 
+	WHERE [ID] = @ID
+END
 
 /*
 Stored Procedures PRODUCT
 */
 
 
+/* AddProduct */
 GO
 CREATE PROCEDURE AddProduct
 	(@Name NVARCHAR(50), @Weight FLOAT, @Price FLOAT, @Amount FLOAT, @DateOfPackaging DateTime2, @DateOfExpiration DateTime2)
@@ -97,6 +144,7 @@ AS BEGIN
 	(@Name, @Weight, @Price, @Amount, @DateOfPackaging, @DateOfExpiration)
 END
 
+/* EditProduct */
 GO
 CREATE PROCEDURE EditProduct
 	(@ID INT, @Name NVARCHAR(50), @Weight FLOAT, @Price FLOAT, @Amount FLOAT, 
@@ -108,10 +156,31 @@ AS BEGIN
 	WHERE [ID] = @ID
 END
 
+/* DeleteProduct */
 GO
 CREATE PROCEDURE DeleteProduct
 	(@ID INT)
 AS BEGIN
 	DELETE FROM ERP.Product
 	WHERE ID = @ID
+END
+
+/* ShowRawProducts !!!! HER SKAL VI LIGE HAVE KIGGET PÅ NOGET MED HVAD DEN SKAL SØGE EFTER !!!! */
+GO
+CREATE PROCEDURE ShowProducts
+	(@Name NVARCHAR(50), @Weight FLOAT, @Price FLOAT, @Amount FLOAT, @DateOfPackaging DateTime2, @DateOfExpiration DateTime2)
+AS BEGIN
+	SELECT [Name], [Weight], [Price], [Amount], [DateOfPackaging], [DateOfExpiration]
+	FROM ERP.Product
+	WHERE [Name] = @Name
+END
+
+/* ShowRawProduct */
+GO
+CREATE PROCEDURE ShowProduct
+	(@ID INT)
+AS BEGIN
+	SELECT [Name], [Weight], [Price], [Amount], [DateOfPackaging], [DateOfExpiration]
+	FROM ERP.Product 
+	WHERE [ID] = @ID
 END
